7f08693e3cb43909668c0d3b4017ac662d50bdc7
ShengfaZhu <1140973730@qq.com>
2018-05-12 21:28:45 +0800

    next permutation

--- next_permutation.cpp
+++ next_permutation.cpp
@@ -10,29 +10,39 @@
 // 1,1,5 → 1,5,1
 
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
 void nextPermutation(vector<int>& nums) {
-    for (int i = nums.size() - 1; i > 0 ; i--) {
-        if (nums[i] > nums[i - 1]) {
-            int tmp = nums[i - 1];
-            nums[i - 1] = nums[i];
-            nums[i] = tmp;
-            return;
-        }
+    if (nums.empty())
+        return;
+    int i = nums.size() - 2;
+    // 从后向前找到第一个降序排列的数，并记录其中的最小值
+    while(i >= 0 && nums[i] >= nums[i + 1]) {        
+        i--;
     }
-    // reverse into ascending order
-    int sz = nums.size();
-    for (int i = 0; i < sz/ 2; i++) {
-        int tmp = nums[sz - i - 1];
-        nums[sz - i - 1] = nums[i];
-        nums[i] = tmp;
+    cout << "i: " << i << endl;
+    if (i < 0) {//原数列为从前至后的降序
+        sort(nums.begin(), nums.end());
+        return;
+    }
+    else {
+        int j = i + 1;
+        while (j < nums.size() && nums[j] > nums[i]) {
+            j++;
+        }
+        cout << "i : " << i << "\n" << "j : " << j << "\n";
+        int tmp = nums[i];
+        nums[i] = nums[j - 1];
+        nums[j - 1] = tmp;
+        sort(nums.begin() + i + 1, nums.end());
+        return;
     }
 }
 
 int main() {
-    vector<int> nums = {3, 2, 1};
+    vector<int> nums = {5, 1, 1};
     nextPermutation(nums);
     for (int& num : nums)
         cout << num << endl;
