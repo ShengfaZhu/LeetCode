bd680bd92630385e4882b19c39eb521f8a0a74b6
ShengfaZhu <1140973730@qq.com>
2018-05-13 10:53:47 +0800

    search insert position

--- search_insert_position.cpp
+++ search_insert_position.cpp
@@ -4,11 +4,50 @@
 // be if it were inserted in order.
 // You may assume no duplicates in the array.
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
 int searchInsert(vector<int>& nums, int target) {
-    
+    if (nums.empty()) {
+        return 0;
+    }
+    int low = 0, high = nums.size() - 1, ind = -1;
+    if (nums[low] > target) {
+        return 0;
+    }
+    if (nums[high] < target) {
+        return nums.size();
+    }
+    while(low <= high) {
+        if (nums[low] == target) {
+            return low;
+        }
+        if (nums[high] == target) {
+            return high;
+        }
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        }
+        else if (nums[mid] < target) {
+            low = mid + 1;
+        }
+        else 
+            high = mid - 1;
+    }
+    high = high < 0 ? 0 : high;
+    cout << high << endl;
+    while (high < nums.size() && nums[high] < target)
+        high++;
+    return high;
+        
 }
 
 int main() {
+    vector<int> nums = {1,3,5,6};
+    int target = 0;
+    cout << searchInsert(nums, target) << endl;
     system("pause");
     return 0;
 }
