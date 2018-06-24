3c0a0eb0b5e7fdf551021ddb0b69532169d87cdf
ShengfaZhu <1140973730@qq.com>
2018-06-24 13:42:21 +0800

    longest increasing subsequence

---
+++ longest_increasing_subsquence.cpp
@@ -0,0 +1,11 @@
+// 300. Longest Increasing Subsequence
+// using dynamic programming still don't understand this algorithm
+int lengthOfLIS(vector<int>& nums) {
+    vector<int> dp;
+    for (int i = 0; i < nums.size(); i++) {
+        auto it = lower_bound(dp.begin(), dp.end(), nums[i]);
+        if (it == dp.end()) dp.push_back(nums[i]);
+        else *it = nums[i];
+    }
+    return dp.size();
+}
