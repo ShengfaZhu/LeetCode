40e9e30ffdf0fe1ad1ef20093385c3d55ff7bd85
ShengfaZhu <1140973730@qq.com>
2018-07-21 14:39:09 +0800

    0-1 Knapsack with dp

--- ones_and_zeros.cpp
+++ ones_and_zeros.cpp
@@ -33,6 +33,26 @@ int findMaxForm(vector<string>& strs, int m, int n) {
     return dp[strs.size()][m][n];
 }
 
+// much faster solution
+int findMaxForm(vector<string>& strs, int m, int n) {
+    if (strs.empty()|| m < 0 || n < 0) return 0; 
+    vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+    // dynamic programming
+    for (int i = 0; i < strs.size(); i++) {
+        int zeros = 0, ones = 0;
+        for (char c : strs[i]) {
+            if (c == '0') zeros++;
+            if (c == '1') ones++;
+        }
+        for (int j = m; j >= zeros; j--) {
+            for (int k = n; k >= ones; k--) {
+                    dp[j][k] = max(dp[j][k], dp[j-zeros][k-ones] + 1);
+            }
+        }
+    }
+    return dp[m][n];
+}
+
 int main() {
     vector<string> strs = {"10", "0001", "111001", "1", "0"};
     int m = 5, n = 3;
