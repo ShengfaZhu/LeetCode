9c46f77754f558b62d2106acb31567ed25fc8b9a
ShengfaZhu <1140973730@qq.com>
2018-08-28 10:46:20 +0800

    coin change II with dp

---
+++ coin_change_II.cpp
@@ -0,0 +1,14 @@
+// Coin Change 2
+// another excellent dynamic program method
+int change(int amount, vector<int>& coins) {
+    vector<int> dp(amount + 1, 0);
+    dp[0] = 1;
+    for (int j = 0; j < coins.size(); j++) {
+        for (int i = 1; i <= amount; i++) {
+            if (coins[j] <= i) {
+                dp[i] = dp[i] + dp[i - coins[j]];
+            }
+        }
+    }
+    return dp[amount];
+}
